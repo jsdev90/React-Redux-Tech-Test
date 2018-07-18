cf099f267a10c7f13f7239090b27f8b0a8bd6b57
urchindev <mario.as.dev@gmail.com>
2018-07-18 10:20:34 +0100

    Assets

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Routes from '../routes';
+import logo from '../assets/img/logo.png';
+import { container, logoImg } from '../styles/app.scss';
 
 const App = () =>
-    <div>
-        <h1>Filter table</h1>
+    <div className={container}>
+        <img src={logo} className={logoImg}/>
         { Routes }
     </div>;
 
